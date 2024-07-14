178e7c8f99eeced4afc89caa10ccd9159b42c97c
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-07-15 02:04:03 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  console.log(import.meta.env.VITE_URL_KEY);
+  return (
+    <div>
+      <h1 className="bg-red-300">Hello world</h1>
+    </div>
+  );
+};
+
+export default App;
