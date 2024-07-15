fd9384faec60bdce44e2fb2a2046f9bfe7c84131
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-07-15 17:48:51 +0600

    homepage ui header footer done

--- src/components/ui/homepage/Footer.tsx
+++ src/components/ui/homepage/Footer.tsx
@@ -1,11 +1,24 @@
-
-
 const Footer = () => {
-    return (
-        <div>
-            Footer
-        </div>
-    );
+  return (
+    <>
+      <footer className="footer footer-center bg-primary text-primary-content p-10 rounded-lg">
+        <aside>
+          <img className="size-12 rounded-lg" src="" alt="" />
+          <p className="font-bold">
+            Fitness Zone
+            <br />
+            Fitness slogan
+          </p>
+          <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
+        </aside>
+        <nav>
+          <div className="grid grid-flow-col gap-4">
+            Icon
+          </div>
+        </nav>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
--- src/components/ui/homepage/Header.tsx
+++ src/components/ui/homepage/Header.tsx
@@ -1,10 +1,46 @@
-
 const Header = () => {
-    return (
-        <div>
-            header
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          Logo
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            {/* mapping navlinks and showing */}
+            {/* {navLinks.map((navlink, index) => (
+              <li key={index}>
+                <a className="px-4">
+                  <NavLink to={navlink.path}>{navlink.name}</NavLink>
+                </a>
+              </li>
+            ))} */}
+            menu
+          </ul>
         </div>
-    );
+        <div className="flex justify-center items-center navbar-center">
+          <img className="size-7" src="/favicon.png" alt="" />
+          <a className="text-xl font-bold ml-2">
+            Fitness <span className="text-primary">Z</span>one
+          </a>
+        </div>
+      </div>
+      <div className="navbar-center hidden lg:flex navbar-end">
+        <ul className="menu menu-horizontal px-1">
+          {/* mapping navlinks and showing */}
+          {/* {navLinks.map((navlink, index) => (
+            <li key={index}>
+              <a className="px-4">
+                <NavLink to={navlink.path}>{navlink.name}</NavLink>
+              </a>
+            </li>
+          ))} */}
+          menu2
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
